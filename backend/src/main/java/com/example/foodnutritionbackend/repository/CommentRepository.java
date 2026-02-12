4fba14d343a58fcca495975366015d7e8baf1f13
Torchman005 <2318257072@qq.com>
2026-02-12 21:33:24 +0800

    完善了论坛功能

--- backend/src/main/java/com/example/foodnutritionbackend/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.foodnutritionbackend.repository;
-
-import com.example.foodnutritionbackend.model.Comment;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface CommentRepository extends MongoRepository<Comment, String> {
-    List<Comment> findByPostId(String postId);
-}
